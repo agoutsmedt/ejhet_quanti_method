fec88ad6129df733e95ea2b0eaf7afbfed845cc7
Aurélien Goutsmedt <agoutsmedt@hotmail.fr>
2025-08-22 20:25:17 +0200

    merging conflicts in app

--- scripts/launch_app_bibliometrics.R
+++ scripts/launch_app_bibliometrics.R
@@ -4,23 +4,62 @@ source(file.path("scripts", "paths_and_packages.R"))
 
 pacman::p_load(shiny, shinycssloaders)
 
-# load graphs 
-graphs <- readRDS(here::here(data_path, "networks_1970_2014_10_year_windows_0.1_rationality_score.RDS"))
-labels <- readRDS(here::here(data_path, "label_ai_1970_2014_10_year_windows_0.1_rationality_score.RDS"))
+graphs <- readRDS(here::here(data_path, "networks_1960_2014_10_year_windows_0.1_rationality_score.RDS"))
+labels <- readRDS(here::here(data_path, "label_ai_1960_2014_10_year_windows_0.1_rationality_score.RDS"))
 
-# add labels to the list of graphs  
+# add labels to the list of graphs
+
 graphs <- lapply(graphs, function(graph) {
   
   graph <- graph %>% 
     activate(nodes) %>% 
     left_join(labels, by = c("dynamic_cluster_leiden" = "id_col")) %>%
+    arrange(desc(node_size)) %>% 
     rename(color = main_colors) %>% 
-    mutate(nodes_tooltip = paste0(Nom, " \\(", Annee_Bibliographique, "\\) ", Titre) %>% str_remove_all(., "[:punct:]"))
+    mutate(nodes_tooltip = paste0(Nom, " \\(", Annee_Bibliographique, "\\) ", Titre) %>% str_remove_all(., "[:punct:]"),
+           value_col = if_else(is.na(value_col), dynamic_cluster_leiden, value_col)) 
   
   graph <- graph %>% 
     activate(edges) %>%
     rename(color = color_edges)
 })
+
+# Adding references
+nodes <- map(graphs, ~ . %N>% as_tibble()) %>% 
+  bind_rows() %>% 
+  mutate(value_col = if_else(is.na(value_col), dynamic_cluster_leiden, value_col),
+         ID_Art = as.integer(ID_Art)) %>% 
+  distinct(ID_Art, value_col, time_window)
+refs <- read_parquet(here::here(wos_data_path, "all_ref.parquet"), as_data_frame = FALSE) %>% 
+  filter(ID_Art %in% nodes$ID_Art) %>% 
+  select(ID_Art, ItemID_Ref, Annee, Nom, Revue_Abbrege) %>% 
+  collect()
+
+top_refs <- nodes %>% 
+  left_join(refs, by = "ID_Art", relationship = "many-to-many") %>% 
+  filter(ItemID_Ref != 0) %>% 
+  group_by(value_col, time_window, ItemID_Ref) %>% 
+  summarise(n = n(), .groups = "drop") %>% 
+  arrange(time_window, value_col, desc(n)) %>% 
+  group_by(time_window, value_col) %>% 
+  slice_head(n = 10) %>% 
+  ungroup() %>% 
+  filter(n > 1) %>%
+  left_join(refs %>% distinct(ItemID_Ref, Nom, Annee, Revue_Abbrege), by = "ItemID_Ref", relationship = "many-to-many") %>% 
+  distinct(value_col, time_window, ItemID_Ref, nb_cit = n, .keep_all = TRUE) 
+
+top_refs_without_id <- nodes %>%  
+  left_join(refs, by = "ID_Art", relationship = "many-to-many") %>% 
+  filter(ItemID_Ref == 0 & Annee != 0 & Nom != "") %>%
+  group_by(value_col, time_window, Nom, Annee) %>% 
+  summarise(n = n(), .groups = "drop") %>% 
+  arrange(time_window, value_col, desc(n)) %>% 
+  group_by(time_window, value_col) %>% 
+  slice_head(n = 10) %>% 
+  ungroup() %>% 
+  filter(n > 1) %>%
+ # left_join(refs %>% distinct(ItemID_Ref, Nom, Annee, Revue_Abbrege), by = "ItemID_Ref", relationship = "many-to-many") %>% 
+  distinct(value_col, time_window, Nom, Annee, nb_cit = n) 
 
 
 launch_network_app(
@@ -28,6 +67,8 @@ launch_network_app(
     cluster_id = "value_col", 
     cluster_information = c("Titre", "Annee_Bibliographique", "Nom", "node_size"),
     cluster_tooltip = "Click on cluster to see more information",
+    top_references = top_refs,
+    top_references_without_id = top_refs_without_id,
     node_id = "ID_Art",
     node_tooltip = "nodes_tooltip",
     node_size = "node_size",
