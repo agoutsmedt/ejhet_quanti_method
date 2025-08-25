8fdaa01b45b615971693b0d9704ada02d50a0f79
tdelcey <thomdelcey@hotmail.fr>
2025-08-25 15:08:01 +0200

    pictures update

--- scripts/descriptive_statistics_on_fulltext_database.R
+++ scripts/descriptive_statistics_on_fulltext_database.R
@@ -3,25 +3,43 @@
 source(file.path("scripts", "paths_and_packages.R"))
 
 # load fulltext metadata
-metadata <- read_rds(file.path(data_path, "full_metadata_journals_cleaned.rds")) 
+metadata <- read_rds(file.path(data_path, "full_metadata_journals_cleaned.rds"))
+
+metadata |> distinct(refined_sub_type)
 
 # plot distribution of language
 
-metadata |> 
-  mutate(language = str_extract(language, "^[^,]+"),
-         language = ifelse(language %in% c("eng", "ger", "fre", "ita"), language, "other")) |>
-  count(language) |> 
-  mutate(pct = n / sum(n) * 100) |> 
-  ggplot(aes(x = reorder(language, pct), y = pct)) +
+metadata |>
+  mutate(
+    language = str_extract(language, "^[^,]+"),
+    language = ifelse(
+      language %in% c("eng", "ger", "fre", "ita"),
+      language,
+      "other"
+    ),
+    language = recode(
+      language,
+      eng = "English",
+      ger = "German",
+      fre = "French",
+      ita = "Italian",
+      other = "Other"
+    )
+  ) |>
+  count(language) |>
+  mutate(pct = n / sum(n) * 100) |>
+  ggplot(aes(x = reorder(language, pct, decreasing = TRUE), y = pct)) +
   geom_col() +
   # y axis in percentage
   scale_y_continuous(labels = scales::percent_format(scale = 1)) +
   labs(
-    title = "Distribution of languages in the JSTOR fulltext Database",
     x = "Languages",
-    y = "") +
-  theme_minimal()
+    y = ""
+  ) +
+  theme_light(base_size = 14)
 
 # save the plot
-ggsave(file.path(image_path, "language_distribution_fulltext_database.png"), width = 8, height = 6)
-
+ggsave(
+  file.path(image_path_temp, "language_distribution_fulltext_database.png"),
+  width = 9
+)
--- scripts/find_closest_words_to_rationality_neighbor.R
+++ scripts/find_closest_words_to_rationality_neighbor.R
@@ -3,22 +3,34 @@
 # paths and packages
 source(file.path("scripts", "paths_and_packages.R"))
 
-# load paragraphs 
-raw_paragraphs <- read_parquet(here::here(jstor_raw_data, "paragraphs_with_target_word.parquet"))
+# load paragraphs
+raw_paragraphs <- read_parquet(here::here(
+  jstor_raw_data,
+  "paragraphs_with_target_word.parquet"
+))
 
 # tokenization
 
 paragraphs <- as.data.table(raw_paragraphs)
 paragraphs <- paragraphs[, .(id, window, target_word, publication_year)]
 
-# group by id and create paragraph_id 
+# group by id and create paragraph_id
 paragraphs[, paragraph_id := as.character(seq_len(.N)), by = id]
 # add id to paragraph_id
 paragraphs[, paragraph_id := paste0(id, "_", as.character(paragraph_id))]
 
 # tokenize words in the window and unlist them
-tokens <- paragraphs[, token := tokenizers::tokenize_words(window, strip_numeric = TRUE, lowercase = TRUE)]
-tokens_unnest <- tokens[, .(token = unlist(token)), by = .(paragraph_id, id, publication_year, target_word)]
+tokens <- paragraphs[,
+  token := tokenizers::tokenize_words(
+    window,
+    strip_numeric = TRUE,
+    lowercase = TRUE
+  )
+]
+tokens_unnest <- tokens[,
+  .(token = unlist(token)),
+  by = .(paragraph_id, id, publication_year, target_word)
+]
 
 # add position of each token in the paragraph
 tokens_unnest[, pos := seq_len(.N), by = paragraph_id]
@@ -28,12 +40,12 @@ targets <- tokens_unnest[token == target_word]
 
 # Previous positions
 targets_prev <- copy(targets)
-targets_prev[, pos := pos - 1]   # shift by -1
+targets_prev[, pos := pos - 1] # shift by -1
 targets_prev[, side := "prev"]
 
 # Next positions
 targets_next <- copy(targets)
-targets_next[, pos := pos + 1]   # shift by +1
+targets_next[, pos := pos + 1] # shift by +1
 targets_next[, side := "next"]
 
 # Combine previous and next positions
@@ -42,65 +54,75 @@ targets_all <- rbind(targets_prev, targets_next)
 targets_all <- targets_all[, .(paragraph_id, pos, side)]
 
 # merge by paragraph_id and pos
-tokens_unnest <- merge(tokens_unnest, targets_all, by = c("paragraph_id", "pos"), all.x = TRUE)
+tokens_unnest <- merge(
+  tokens_unnest,
+  targets_all,
+  by = c("paragraph_id", "pos"),
+  all.x = TRUE
+)
 
-# filter non-NA side 
+# filter non-NA side
 tokens_unnest <- tokens_unnest[!is.na(side)]
 
-# estimate the total frequency of neighbor words by side 
+# estimate the total frequency of neighbor words by side
 neighbor_words_freq <- tokens_unnest[, .N, by = .(token, side, target_word)]
 
 # filter stopwords
-neighbor_words_freq <- neighbor_words_freq[!token %in% stopwords::stopwords("en")]
+neighbor_words_freq <- neighbor_words_freq[
+  !token %in% stopwords::stopwords("en")
+]
 
 # save results in rds
-saveRDS(neighbor_words_freq, file = here::here(data_path, "neighbor_target_words_freq.rds"))
+saveRDS(
+  neighbor_words_freq,
+  file = here::here(data_path, "neighbor_target_words_freq.rds")
+)
 
 
 # Now plot each most frequent words by decade
 
-
-# first delete stopwords and target words 
+# first delete stopwords and target words
 
 tokens_unnest <- tokens_unnest[!token %in% stopwords::stopwords("en")]
-tokens_unnest <- tokens_unnest[!str_detect(token, "^\\d+$")]  # remove numeric tokens
+tokens_unnest <- tokens_unnest[!str_detect(token, "^\\d+$")] # remove numeric tokens
 
 # Plot the most frequent words by decade
 
 # Définir les target words
 targets <- list(
   rationality = "rationality",
-  rational    = "rational",
-  both        = c("rationality", "rational")
+  rational = "rational",
+  both = c("rationality", "rational")
 )
 
 # Boucle
 for (name in names(targets)) {
-  
-  p <- tokens_unnest |> 
+  p <- tokens_unnest |>
     filter(target_word %in% targets[[name]]) |>
     mutate(decade = floor(publication_year / 10) * 10) |>
     count(token, decade) |>
-    filter(decade >= 1900) |> 
-    group_by(decade) |> 
-    slice_max(n, n = 10, with_ties = FALSE) |> 
+    filter(decade >= 1900) |>
+    group_by(decade) |>
+    slice_max(n, n = 5, with_ties = FALSE) |>
     mutate(token = reorder_within(token, n, decade)) |>
     ggplot(aes(x = token, y = n)) +
-    geom_col(fill = "#2C77B8") +
-    facet_wrap(~ decade, scales = "free") +
+    geom_col() +
+    facet_wrap(~decade, scales = "free") +
     labs(
-      title = paste("Most Frequent neighbor words by decade -", name),
       x = "Words",
       y = "Frequency"
     ) +
     coord_flip() +
     scale_x_reordered() +
-    theme_light() 
-  
+    theme_light(base_size = 20)
+
   # sauvegarde
   ggsave(
     here::here(image_path_temp, paste0("Top_neighbor_words_", name, ".png")),
     plot = p,
-    width = 60, height = 40, units = "cm", dpi = 300
+    width = 60,
+    height = 40,
+    units = "cm",
+    dpi = 300
   )
 }
